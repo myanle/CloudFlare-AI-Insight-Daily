fff4c159def73e3f5c63987efbb0c2ca188335f1
何夕2077 <274166795@qq.com>
2025-06-19 20:22:27 +0800

    Update toc.js

--- today/book/toc.js
+++ today/book/toc.js
@@ -68,3 +68,10 @@ class MDBookSidebarScrollbox extends HTMLElement {
     }
 }
 window.customElements.define("mdbook-sidebar-scrollbox", MDBookSidebarScrollbox);
+
+(function(c,l,a,r,i,t,y){
+    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window, document, "clarity", "script", "s0drt9kmxf");
+
